1326e4dd8f1573b67622a4262fd3cda040ad965e
Ginnie313 <whiteg@carleton.edu>
2019-06-01 19:16:09 -0500

    first effort at coding terrain

--- src/Terrain.java
+++ src/Terrain.java
@@ -1,4 +1,9 @@
 public class Terrain {
+    ArrayList<int> moveCostArray;
+    ArrayList<boolean> isPassableArray;
+    boolean eventOnStartTurn;
+    boolean eventOnEnter;
+    boolean eventOnEndTurn;
     public Terrain() {
 
     }
