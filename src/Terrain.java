00b40e0f02581a9a413d43cb87c0d30d51bdc7a1
Ginnie313 <whiteg@carleton.edu>
2019-06-01 19:21:38 -0500

    Setters and getters

--- src/Terrain.java
+++ src/Terrain.java
@@ -1,10 +1,52 @@
+import java.util.*;
 public class Terrain {
-    ArrayList<int> moveCostArray;
-    ArrayList<boolean> isPassableArray;
+    ArrayList<Integer> moveCostArray;
+    ArrayList<Boolean> isPassableArray;
     boolean eventOnStartTurn;
     boolean eventOnEnter;
     boolean eventOnEndTurn;
+
     public Terrain() {
 
+    }
+
+    public ArrayList<Boolean> getIsPassableArray() {
+        return isPassableArray;
+    }
+
+    public void setIsPassableArray(ArrayList<Boolean> isPassableArray) {
+        this.isPassableArray = isPassableArray;
+    }
+
+    public ArrayList<Integer> getMoveCostArray() {
+        return moveCostArray;
+    }
+
+    public void setMoveCostArray(ArrayList<Integer> moveCostArray) {
+        this.moveCostArray = moveCostArray;
+    }
+
+    public boolean isEventOnStartTurn() {
+        return eventOnStartTurn;
+    }
+
+    public void setEventOnStartTurn(boolean eventOnStartTurn) {
+        this.eventOnStartTurn = eventOnStartTurn;
+    }
+
+    public boolean isEventOnEnter() {
+        return eventOnEnter;
+    }
+
+    public void setEventOnEnter(boolean eventOnEnter) {
+        this.eventOnEnter = eventOnEnter;
+    }
+
+    public boolean isEventOnEndTurn() {
+        return eventOnEndTurn;
+    }
+
+    public void setEventOnEndTurn(boolean eventOnEndTurn) {
+        this.eventOnEndTurn = eventOnEndTurn;
     }
 }
