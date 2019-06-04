9b1538ea1dff57a45df4637fcd9d80fc51331454
Ginnie313 <whiteg@carleton.edu>
2019-06-03 19:34:31 -0500

    Created a Model constructor

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,11 @@
 import java.util.*;
 import java.lang.Math;
 public class Model {
+    private ArrayList<Creature> creatures;
+    public MapModel mapModel;
 
+    public Model(int x, int y, int z){
+        creatures = new ArrayList<>();
+        mapModel = new MapModel(creatures, x, y, z);
+    }
 }
